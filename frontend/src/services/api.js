f5b04705715ac9d75e326083d4771d6a0044dfd8
Andres Arguelles <arguellesandres03@gmail.com>
2025-03-18 14:29:38 -0500

    Subiendo frontend y backend correctamente

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://pizzeria-web.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL || "https://pizzeria-web.onrender.com/",
 });
 
 // Interceptor para agregar el token en cada petición
